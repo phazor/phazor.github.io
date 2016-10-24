43c7b4f04417ee9cb6c2dd5cf565593b4c420083
phazor <tristan.r.read@gmail.com>
2016-10-25 00:49:39 +0200

    add About page placeholder

---
+++ src/views/About/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import MenuBar from '../../components/MenuBar/MenuBar';
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <MenuBar></MenuBar>
+        <div className="About">
+          <h1>About!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
