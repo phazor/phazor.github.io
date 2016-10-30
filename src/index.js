9211a6bcac0575fb3051f6d4090a86746284bd54
phazor <tristan.r.read@gmail.com>
2016-10-30 17:54:33 +0100

    add basename to the server url

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
 
 import Routes from './routes';
 import '../node_modules/milligram/dist/milligram.min.css';
 import './index.css';
 
+import { useRouterHistory } from 'react-router';
+import { createHistory } from 'history';
+
+const history = useRouterHistory(createHistory)({
+  basename: '/my-cool-single-page-app'
+});
+
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <Routes history={history} />,
   document.getElementById('root')
 );
