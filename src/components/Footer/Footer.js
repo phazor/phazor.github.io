24182d919e4f5f6dc169a2261a942203030cfdff
phazor <tristan.r.read@gmail.com>
2016-10-31 15:32:08 +0100

    styling tweaks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,12 @@ import './Footer.css';
 class Footer extends Component {
   render() {
     return (
-      <section className="footer">
-        <p><i><a href="http://www.flaticon.com/free-icon/rocket_178158">Rocket icon</a> designed by </i>Flat Icons<i> from Flaticon</i></p>
+      <section className="Footer">
+        <ul>
+          <li><i><a href="http://www.flaticon.com/free-icon/rocket_178158">Rocket icon</a> designed by </i>Flat Icons<i> from Flaticon</i></li>
+          <li className="separator">|</li>
+          <li>Copyright <i>Tristan Read</i> 2016</li>
+        </ul>
       </section>
     )
   }
