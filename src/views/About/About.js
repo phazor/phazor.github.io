2f929783c7e05030f92daf9d0f826cea30174d5b
phazor <tristan.r.read@gmail.com>
2016-11-04 14:57:55 +0100

    add CV to about-me page
    note: this uses auto-resizer

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { iframeResizer } from 'iframe-resizer';
 import './About.css';
 
+
 class About extends Component {
+  componentDidMount() {
+    iframeResizer({
+        checkOrigin:false,
+        autoResize:false
+      }, '#iframe'
+    );
+  }
+
   render() {
     return (
-        <section className="About container">
-          <h1>About!</h1>
-        </section>
+      <section className="About">
+        <iframe id="iframe" src="https://phazor.github.io/about-me/" width="100%" onClick={this.resize}></iframe>
+      </section>
     );
   }
 }
