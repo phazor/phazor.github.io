0a53b802da0ac8560fd325be90b6d4f56d52e9d7
phazor <tristan.r.read@gmail.com>
2016-11-12 15:39:33 +0100

    styling tweaks

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -4,7 +4,7 @@ import './MenuItem.css';
 
 const MenuItem = (props) => {
   return (
-    <li className="MenuItem"><Link onClick={props.handleClick} to={props.to}>{props.children}</Link></li>
+    <li className="MenuItem"><Link onClick={props.handleClick} to={props.to} activeClassName="active">{props.children}</Link></li>
   )
 }
 
