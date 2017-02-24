73522e765be76cba6ece9aace3cbec9b6728b6c8
phazor <tristan.r.read@gmail.com>
2017-02-25 00:53:04 +0100

    add fps monitor

--- src/views/Planets/Planets.js
+++ src/views/Planets/Planets.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 
+let frames_per_sec = 5;
+
 class Planets extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateFPS = this.updateFPS.bind(this);
+    this.state = {};
+  }
+
+  updateFPS() {
+    this.setState({ fps: frames_per_sec });
+    // frames_per_sec++;
+  }
 
   componentDidMount() {
     renderScene()
+    this.fpsID = setInterval(this.updateFPS, 100)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fpsID);
   }
 
   render() {
     return (
       <div id="canvasWrapper" style={{
-        textAlign: 'center'
+        textAlign: 'center',
+        marginTop: '2rem'
       }}>
-        <p>Trappist</p>
+        <h3>Trappist</h3>
+        <FPS fps={this.state.fps} />
         <div id="canvasWrapper" style={{
           display: 'inline-block'
-        }}></div>
+        }}>
+        </div>
       </div>
     )
   }
 }
 
+const FPS = ({fps}) => (
+  <p>fps: {fps}</p>
+);
+
 function renderScene() {
+  let last = 0;
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -41,8 +67,17 @@ function renderScene() {
     cube.rotation.x += 0.1;
     cube.rotation.y += 0.1;
   	renderer.render( scene, camera );
+    fpsCalc();
   }
   render();
+
+  function fpsCalc() {
+    let now = performance.now();
+    let fps = 1000 / (now - last);
+    last = now;
+
+    frames_per_sec = Math.floor(fps, 10);
+  }
 }
 
 export default Planets;
