4e8e73df6cea8c876edac66ff3b920a880058e41
phazor <tristan.r.read@gmail.com>
2017-02-25 03:10:31 +0100

    new system for adding planets to a scene

--- src/views/Planets/Planets.js
+++ src/views/Planets/Planets.js
@@ -6,6 +6,7 @@ const OrbitControls = Three_OrbitControls(THREE);
 
 let frames_per_sec = 5;
 
+// Planets Component
 class Planets extends Component {
   constructor(props) {
     super(props);
@@ -44,36 +45,67 @@ class Planets extends Component {
   }
 }
 
+// FPS Component
 const FPS = ({fps}) => (
   <p>fps: {fps}</p>
 );
 
 function renderScene() {
+  let addToScene = (scene, object) => {
+    let start = new THREE.Vector3(...object.start);
+    let mesh = new THREE.Mesh(
+      new (Function.prototype.bind.apply(object.geometryType, [null, ...object.geometry]))(),
+      new (Function.prototype.bind.call(object.materialType, null, object.material))()
+    // ).translateOnAxis(
+    //   start.normalize(),
+    //   start.length()
+    )
+    scene.add(mesh);
+    mesh.position.set(...object.start);
+  }
+
+  let trappist_1 = {
+    a: {
+      geometryType: THREE.SphereGeometry,
+      geometry: [ 0.6, 16, 16 ],
+      materialType: THREE.MeshBasicMaterial,
+      material: { color: 0xff0000 },
+      start: [0, 0, 0]
+    },
+    b: {
+      geometryType: THREE.SphereGeometry,
+      geometry: [ 0.2, 16, 16 ],
+      materialType: THREE.MeshBasicMaterial,
+      material: { color: 0x00ff00 },
+      start: [5, 0, 0]
+    }
+  }
+
   // Used for FPS Calcs
   let last = 0;
   let controls;
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
   document.getElementById('canvasWrapper').appendChild( renderer.domElement );
 
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
+  addToScene(scene, trappist_1.a);
+  addToScene(scene, trappist_1.b);
+  camera.position.z = 10;
 
   controls = new OrbitControls( camera, renderer.domElement );
+
+  render();
+
+  //Functions
 
   function render() {
   	requestAnimationFrame( render );
   	renderer.render( scene, camera );
     fpsCalc();
   }
-  render();
 
   function fpsCalc() {
     let now = performance.now();
