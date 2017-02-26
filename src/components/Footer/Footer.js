567ba8c784d85051383d558ff8f55d0e9cc520e9
phazor <tristan.r.read@gmail.com>
2017-02-27 00:41:16 +0100

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,11 @@ class Footer extends Component {
     return (
       <section className="Footer">
         <ul>
-          <li><i><a href="http://www.flaticon.com/free-icon/rocket_178158">Rocket icon</a> designed by </i>Flat Icons<i> from Flaticon</i></li>
+          <li><i><a href="http://www.flaticon.com/free-icon/rocket_178158">Rocket icon</a> from Flaticon</i></li>
           <li className="separator">|</li>
-          <li>Copyright <i>Tristan Read</i> 2016</li>
+          <li><i><a href="https://www.eso.org/public/images/eso0932a/">Milky Way Panorama</a> by </i>ESO/S. Brunier<i> from Flaticon</i></li>
+          <li className="separator">|</li>
+          <li>Copyright <i>Tristan Read</i> 2017</li>
         </ul>
       </section>
     )
