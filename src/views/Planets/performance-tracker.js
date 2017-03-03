1cf857dbe810fa24a23140e8f8bcd1391658ccbb
phazor <tristan.r.read@gmail.com>
2017-03-03 21:28:34 +0100

    add performance tracker

---
+++ src/views/Planets/performance-tracker.js
@@ -0,0 +1,37 @@
+// Prints performance data to the console.
+// Compares to an `ideal` 60fps performance.
+//
+// Returns:
+//   % thread time spent in loop averaged over the last second
+//   # frame skips per second
+//   longest frame per second
+//
+// Usage:
+// 1. Initialize with `new performanceTracker()`
+// 2. use performance.now() for recording start and end times
+// 3. call tracker with the result of `end` - `start`
+//
+// e.g:
+//
+// const tracker = performanceTracker(60)
+// 60fpsLoop {
+//   let start = performance.now()
+//   doSomething()
+//   let end = performance.now()
+//   tracker(end - start)
+// }
+const performanceTracker = () => {
+  let values = [];
+  return function(value) {
+    values.push(value);
+    if (values.length === 60) {
+      let average = values.reduce((a, b) => (a + b)) / values.length; // This is in milliseconds
+      console.log(`average execution time: ${100 * (average * 60) / 1000}%`)
+      console.log(`frame skips: ${values.filter((a) => (a > (1000 / 60))).length}`);
+      console.log(`longest frame: ${values.reduce((a, b) => (a > b ? a : b))}ms`)
+      values = [];
+    }
+  }
+}
+
+export default performanceTracker;
