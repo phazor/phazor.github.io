2ac2b497095898250457599f544f3da5cfdb6f31
phazor <tristan.r.read@gmail.com>
2017-03-04 19:03:47 +0100

    add test for Home section

--- src/views/Home/Home.test.js
+++ src/views/Home/Home.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Home from './Home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Home />, div);
 });
+
+it('Home contains section element', () => {
+  let wrapper = shallow(<Home />);
+  expect(wrapper.find('section').length).toEqual(1);
+})
