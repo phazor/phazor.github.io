2f8052e724eed5e4e44e66ed0ee70f2997f1d1e3
phazor <tristan.r.read@gmail.com>
2017-03-06 18:20:34 +0100

    reword NEO tests

--- src/reducers/NEO.test.js
+++ src/reducers/NEO.test.js
@@ -1,30 +1,30 @@
 import reducer from './NEO'
 
-it('reducer keeps state with no action', () => {
+it('keeps state with no action', () => {
   let state = reducer({ test: 'Test' }, {});
-  
+
   expect(state.test).toEqual('Test');
 });
 
-it('reducer honors initial state when no state is passed in', () => {
+it('honors initial state when no state is passed in', () => {
   let state = reducer(undefined, {});
 
   expect(state.lastAction).toEqual('NONE');
 });
 
-it('reducer applies lastAction upon fetch request', () => {
+it('applies lastAction upon fetch request', () => {
   let state = reducer({}, { type: 'FETCH_NEO_REQUEST' });
 
   expect(state.lastAction).toEqual('FETCH_NEO_REQUEST');
 });
 
-it('reducer applies lastAction upon fetch success', () => {
+it('applies lastAction upon fetch success', () => {
   let state = reducer({}, { type: 'FETCH_NEO_SUCCESS' });
 
   expect(state.lastAction).toEqual('FETCH_NEO_SUCCESS');
 });
 
-it('reducer applies lastAction and reason upon fetch failure', () => {
+it('applies lastAction and reason upon fetch failure', () => {
   let state = reducer({}, {
     type: 'FETCH_NEO_FAILURE',
     reason: { stack: "Error: 404" }
